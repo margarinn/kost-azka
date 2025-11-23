40b95d68d9ca061bdbddfa63f03f654c83aa767f
margarine <rintanaru@proton.me>
2025-11-23 10:44:39 +0700

    change logo in the tab

--- resources/views/landing_page/landing.blade.php
+++ resources/views/landing_page/landing.blade.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Azka Living</title>
+    {{-- favicon for landing page --}}
+    <link rel="icon" type="image/png" href="{{ asset('foto/LogoKost.png') }}">
+    <link rel="shortcut icon" href="{{ asset('foto/LogoKost.png') }}">
 
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        {{-- Favicon: use LogoKost.png from public/foto so browser tab shows your logo --}}
+        <link rel="icon" type="image/png" href="{{ asset('foto/LogoKost.png') }}">
+        <link rel="shortcut icon" href="{{ asset('foto/LogoKost.png') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
