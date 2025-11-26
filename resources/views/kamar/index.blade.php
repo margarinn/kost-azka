07553f35c387f364935ef550ca7d40abb5fa0ed1
Hotniels <diasi130706@gmail.com>
2025-11-27 00:18:37 +0700

    fix tipe kamar

--- resources/views/kamar/index.blade.php
+++ resources/views/kamar/index.blade.php
@@ -113,7 +113,6 @@
                                     <tr>
                                         <td colspan="6" class="px-6 py-8 whitespace-nowrap text-center text-gray-500">
                                             <div class="flex flex-col items-center justify-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-gray-300 mb-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" /></svg>
                                                 <p class="text-lg font-medium text-gray-900">Belum ada data kamar</p>
                                                 <p class="text-sm text-gray-500">Silakan tambahkan kamar baru atau ubah filter pencarian Anda.</p>
                                             </div>
